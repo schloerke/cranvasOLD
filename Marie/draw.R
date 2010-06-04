84f221bfe40bc50c085d04b2134232183002c34e
mariev <mariev@iastate.edu>
2010-06-03 22:55:35 -0500

    helper functions

--- Marie/draw.R
+++ Marie/draw.R
@@ -39,30 +39,58 @@ make_pretty_axes <- function(dataRange, minimum, maximum)
 
 #draws grid background as a single layer
 draw_grid_with_positions <- function(plotObj, dataRange, horiPos=NULL, vertPos=NULL){
-    linewidthX<-round(0.002*(dataRange[2]-dataRange[1]),3)
-	linewidthY<-round(0.002*(dataRange[4]-dataRange[3]),3)
-	intervalX<-(dataRange[2]-dataRange[1])/(length(horiPos)+1)
-	intervalY<-(dataRange[4]-dataRange[3])/(length(vertPos)+1)
+	dims<-get_dims(dataRange,size=0.002,horiPos,vertPos)
+    left<-get_left(dataRange,dims[3],vertPos)
+	right<-get_right(dataRange,dims[3],dims[1],vertPos)
+	bottom<-get_bottom(dataRange,dims[4],horiPos)	
+	top<-get_top(dataRange,dims[3],dims[2],horiPos)
+	print(left)
+	plotObj$add_layer(rect(left=left,right=right,bottom=bottom,top=top,fill="grey80",stroke="grey80"))
 
+}
+
+get_dims<-function(dataRange,size=0.002,horiPos=NULL,vertPos=NULL){
+	return(c(get_linewidth(data=dataRange[1:2],size=0.002),
+			 get_linewidth(data=dataRange[3:4],size=0.002),
+			 get_interval(data=dataRange[1:2],number=horiPos),
+			 get_interval(data=dataRange[3:4],number=vertPos)))
+}
 	
-	left<-round(rep(seq(from=dataRange[1],to=dataRange[2]-intervalX,by=intervalX),length(vertPos)+1),3)
-	right<-round(rep(seq(from=dataRange[1]+intervalX-linewidthX,to=dataRange[2],by=intervalX),length(vertPos)+1),3)
 	
+	
+	
+get_interval<-function(data,number=NULL){
+	return((data[2]-data[1])/(length(number)+1))
+}
+
+get_linewidth<-function(data,size=0.002){
+	return(round(size*(data[2]-data[1]),3))
+}
+
+get_left<-function(dataRange,intervalX,vertPos){
+	return(round(rep(seq(from=dataRange[1],to=dataRange[2]-intervalX,by=intervalX),length(vertPos)+1),3))
+}
+
+get_right<-function(dataRange,intervalX,linewidthX,vertPos){
+	return(round(rep(seq(from=dataRange[1]+intervalX-linewidthX,to=dataRange[2],by=intervalX),length(vertPos)+1),3))
+}
+
+get_bottom<-function(dataRange,intervalY,horiPos){
 	temp<-seq(from=dataRange[3], to=dataRange[4]-intervalY,by=intervalY)
 	bottom<-NULL
 	for (i in 1:length(temp)){
 		bottom<-c(bottom,rep(temp[i],(length(horiPos)+1)))
 	}
-	bottom<-round(bottom,3)
-	
+	return(round(bottom,3))
+}
+
+get_top<-function(dataRange, intervalY,linewidthY,horiPos){
 	temp<-seq(from=dataRange[3]+intervalY-linewidthY,to=dataRange[4],by=intervalY)
 	top<-NULL
 	for(i in 1:length(temp)){
 		top<-c(top,rep(temp[i],(length(horiPos)+1)))
 	}
-	top<-round(top,3)
-	plotObj$add_layer(rect(left=left,right=right,bottom=bottom,top=top,fill="grey80",stroke="grey80"))
-
+	return(round(top,3))
 }
 
 #no changes
@@ -174,7 +202,56 @@ draw_y_axes_with_labels <- function(plotObj, dataRanges, axisLabels, labelVertPo
 make_new_plot <- function(x=600,y=600,windowRanges){
 	new_plot(x,y, xrange=windowRanges[1:2], yrange=windowRanges[3:4])
 }	
-		
+
+#no changes
+make_data_ranges<-function(dataColumn){
+#  return(c(min(dataColumn)-0.25, max(dataColumn)+0.25))
+	return(c(min(dataColumn)-0.01, max(dataColumn)+0.01))
+#  return(range(dataColumn))
+}
+
+#no changes
+bprint <- function(...) {
+	if(if_bprint()) {
+		cat(substitute(...),":\n")
+		str(...)
+	}
+}
+if_bprint <- function()
+{
+	return(TRUE)
+}
+make_window_ranges <- function(dataRanges, xlab=NULL, ylab=NULL)
+{
 	
+	if(!is.null(ylab))
+    xmin = dataRanges[1] - 0.2*diff(dataRanges[1:2])
+	else
+    xmin = dataRanges[1] - 0.13*diff(dataRanges[1:2])
+	
+	xmax = dataRanges[2]+0.1*diff(dataRanges[1:2])
+	
+	
+	if(!is.null(xlab))
+    ymin = dataRanges[3]-0.2*diff(dataRanges[3:4])
+	else
+    ymin = dataRanges[3]-0.13*diff(dataRanges[3:4])
+    
+	ymax = dataRanges[4]+0.1*diff(dataRanges[3:4])
+	
+	
+	windowRanges <- c(
+					  xmin,
+					  xmax,
+					  ymin,
+					  ymax
+					  )
+	
+	
+	if(if_bprint())
+    cat("Window Range: x=c(", windowRanges[1],", ",windowRanges[2],")  y=(",windowRanges[3],", ",windowRanges[4],")\n")
+	
+	windowRanges  
+}
 	
 	    
