ce86f0469b7a5712efebb64824ae2bc3969aba48
mariev <mariev@iastate.edu>
2010-06-09 15:30:54 -0500

    faceting labels added

--- Marie/facet_demo.R
+++ Marie/facet_demo.R
@@ -1,13 +1,17 @@
 library(qtpaint)
+library(ggplot2)
 
 setwd("/Users/marie/")
 source("Documents/cranvas/Marie/faceting.R")
 source("Documents/cranvas/Marie/api-sketch.r")
 source("Documents/cranvas/Marie/draw.R")
 
-#matches ggplot2 example
-library(ggplot2)
-qplot(mpg,wt,data=mtcars,facets=vs ~gear)
+#matches ggplot2 examples
 
+
+qplot(mpg,wt,data=mtcars,facets=vs~gear)
+qtfacet(x=1,y=6,data=mtcars,facets=c(10,8))
+
+
+qplot(mpg,wt,data=mtcars,facets=gear~vs)
 qtfacet(x=1,y=6,data=mtcars,facets=c(8,10))
-	
