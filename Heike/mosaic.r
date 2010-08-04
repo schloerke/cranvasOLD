ded176b46aa4d165e5eb6efac54d4d3314f7813d
Heike Hofmann <hofmann@iastate.edu>
2010-08-03 20:09:49 -0700

    interactive keys for mosaics

--- Heike/mosaic.r
+++ Heike/mosaic.r
@@ -4,6 +4,7 @@ source("utilities/axes.r")
 require(stringr)
 require(productplots)
 
+source("../Heike/hadley-productplots-83f5772/R/parse.r")
 
 find_x_label <- function(df) {
   vars <- setdiff(names(df), c(".wt", "l", "r", "t", "b", "level"))
@@ -21,9 +22,15 @@ find_y_label <- function(df) {
   paste(vars[sapply(vars, function(x) return(length(unique(axis.set[,x]))>1))],"")
 }
 
-qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = TRUE, na.rm = FALSE, subset=NULL, colour="grey30",...) {
-  data <- productplots:::prodcalc(data, formula, divider, cascade, scale_max, na.rm = na.rm)
-  
+
+qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = TRUE, na.rm = FALSE, subset=NULL, colour="grey30", ...) {
+  odata <- data
+  data <- productplots:::prodcalc(odata, formula, divider, cascade, scale_max, na.rm = na.rm)
+  if (is.null(data$hilite)) data$hilite <- FALSE
+
+
+#browser()
+
   if (!missing(subset)) {
     sel <- eval(substitute(subset), data, parent.frame())
     data <- data[sel & !is.na(sel), ]
@@ -40,7 +47,6 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
   left <- data$l
   right <- data$r
 
-  if (is.null(data$hilite)) data$hilite <- FALSE
   
   xlab <- find_x_label(data)
   ylab <- find_y_label(data)
@@ -57,6 +63,8 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
   if (!is.na(row))
   	ylabels <- productplots:::row_labels(data[data$level == row, ])
 
+#  if (is.null(main)) main <- as.character(formula)
+  
   windowRanges <- make_window_ranges(dataRanges, xlab, ylab, ytickmarks=ylabels)
 
   lims <- qrect(windowRanges[c(1,2)], windowRanges[c(3,4)])
@@ -107,20 +115,22 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
 	  x <- .queryPos[1]
 	  y <- .queryPos[2]
 
-	  info <- subset(data, (y <= t) & (y >= b) & (x <= r) & (x >=l) & (level == max(level)))
+	  info <- subset(data, (y <= t) & (y >= b) & (x <= r) & (x >=l) & (level == .level))
 #	  print(str(info))
 	  if (nrow(info)>0) {
-	    idx <- setdiff(names(data),c("l","t","r","b", ".wt","level", "hilite"))
+	    idx <- setdiff(names(data),c("l","t","r","b", ".wt","level", "hilite"))[1:.level]
 	    
 	    infostring <- character()
-	    infostring <- paste(idx, info[,idx],collapse="\n", sep=":")
+	    infodata <- as.character(unlist(info[1,idx]))
+	    infostring <- paste(idx, infodata,collapse="\n", sep=":")
+#	    print(infodata)
 	    
    	    qstrokeColor(painter) <- "white"
 		qdrawText(painter,
 		  infostring,
 		  x, y, 
 		  valign="top",
-		  halign="center")
+		  halign="left")
 	  }  
   }
   
@@ -140,6 +150,8 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
   }
   
   hilite <- function(item, painter, exposed, ...) {
+	if (!is.null(data$hilite)) {
+
 	hdata <- subset(data, (hilite==TRUE) & (level == .level))
 	if (nrow(hdata)>0) {
 	
@@ -155,7 +167,7 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
 		top, 
 		fill="darkred")
     }
-    
+    }   
 	if (!is.null(.queryPos)) {
 		drawInfoString(item, painter, exposed)
 	}
@@ -168,7 +180,7 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
 	
   hover <- function(item, event, ...) { 
     .queryPos <<- as.numeric(event$pos())
-    bprint(.queryPos)
+#    bprint(.queryPos)
 	qupdate(hilitelayer)
   }
 
@@ -216,6 +228,7 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
     .startBrush <<- NULL
     .endBrush <<- NULL
   }
+
   
   keyPressFun <- function(item, event, ...) {
 	print(event$key())
@@ -224,12 +237,105 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
 	# down - arrow: 16777237
 	# right: 16777236
 	# left: 16777234
+
+	datachanged <- FALSE
+	formulachanged <- FALSE
+	form <- parse_product_formula(formula)
 	
 	if (key == 16777235) {		# arrow up
 	  if (.level > 1) .level <<- .level - 1
 	}
 	if (key == 16777237) {		# arrow down
 	  if (.level < max(data$level)) .level <<- .level + 1
+	}
+	if (key == 16777234) {		# arrow left
+	# move variable into mosaic plot from left
+	  if (.level < max(data$level)) {
+	    lindx <- max(data$level)-.level+1
+	 
+	    vars <- form$marg
+		form$marg <- rev(c(rev(vars[-lindx]),vars[lindx]))
+
+		formulachanged <- TRUE
+	  }
+	}
+
+	if (key == 67) { 	# 'c' or 'C' for 'condition'
+		lindx <- max(data$level)-.level +1
+	#  if (.level < max(data$level)) {
+	    bprint(formula)
+	    vars <- form$marg[1:lindx]
+	    form$cond <- c(setdiff(form$marg, vars),form$cond)
+		form$marg <- vars
+
+		bprint(formula)
+		formulachanged <- TRUE
+	#  }
+
+#browser()
+	}
+
+	if (key == 85) { 	# 'u' or 'U' for 'unconditioning'
+	#  if (lindx < max(data$level)) {
+	    form$marg <- c(form$marg, form$cond)
+	    form$cond <- character(0)
+
+		formulachanged <- TRUE
+	#  }
+
+#browser()
+	}
+
+	if (key == 82) { 	# 'r' or 'R' for 'rotate'
+		lindx <- max(data$level)-.level + 1
+
+	  if (divider[lindx] %in% c('hbar','vbar')) 
+	  	divider[lindx] <<- setdiff(c('hbar','vbar'),divider[lindx])
+	  if (divider[lindx] %in% c('hspine','vspine')) 
+	  	divider[lindx] <<- setdiff(c('hspine','vspine'),divider[lindx])
+	  
+		datachanged <- TRUE
+
+#browser()
+	}
+	if (key == 83) { 	# 's' or 'S' for 'spine'
+			lindx <- max(data$level)-.level + 1
+	
+	  if (divider[lindx] == 'vbar') 
+	  	divider[lindx] <<- 'vspine'
+	  if (divider[lindx] == 'hbar') 
+	  	divider[lindx] <<- 'hspine'
+	  
+		datachanged <- TRUE
+	}
+
+	if (key == 66) { 	# 'b' or 'B' for 'bar'
+			lindx <- max(data$level)-.level + 1
+	
+	  if (divider[lindx] == 'vspine') 
+	  	divider[lindx] <<- 'vbar'
+	  if (divider[lindx] == 'hspine') 
+	  	divider[lindx] <<- 'hbar'
+
+		datachanged <- TRUE
+	}
+
+	if (formulachanged) {
+		bprint(formula)
+		print(paste(form$wt,collapse= "+"))
+		print(paste(form$marg,collapse= "+"))
+		print(paste(form$cond,collapse= "+"))
+		formstring <- paste(form$wt,"~", paste(form$marg, collapse= "+"))
+		if (length(form$cond) > 0) formstring <- paste(formstring, "|", paste(form$cond, collapse= "+"))
+		formula <<- as.formula(formstring)
+		bprint(formula)
+
+		datachanged <- TRUE
+	}
+	
+	if (datachanged) {
+	  data <<- productplots:::prodcalc(odata, formula, divider, cascade, scale_max, na.rm = na.rm)
+      if (is.null(data$hilite)) data$hilite <<- FALSE	
 	}
 
 	# should be updating the data set, then start all fresh ...
@@ -257,122 +363,7 @@ qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = 
   view
 }
 
-qtmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = TRUE, na.rm = FALSE, subset, ...) {
-  res <- productplots:::prodcalc(data, formula, divider, cascade, scale_max, na.rm = na.rm)
-  
-  if (!missing(subset)) {
-    sel <- eval(substitute(subset), res, parent.frame())
-    res <- res[sel & !is.na(sel), ]
-  }
-  res <<- res
-  qtmosaic.draw(res, ...)
-}
+plot1 <- qmosaic(happy, ~ health+sex+happy, c("vspine","hspine","hspine"))  
+print(plot1)
 
-
-
-qtmosaic.draw <- function(data, colour="grey30", highlight="red", alpha=1, ...) {
-  # position of label
-  .queryPos <- NULL
-
-  top <- data$t
-  bottom <- data$b
-  left <- data$l
-  right <- data$r
-
-  xlab <- find_x_label(data)
-  ylab <- find_y_label(data)
-
-  dataRanges <- c(make_data_ranges(c(min(left), max(right))),make_data_ranges(c(min(bottom),max(top))))
-  bprint(dataRanges)
-
-# space in window around plot (margins in base R)  
-# this space depends on the labels needed on the left
-# find out about these first:
-
-  row <- productplots:::find_row_level(data)
-  ylabels <- NULL
-  if (!is.na(row))
-  	ylabels <- productplots:::row_labels(data[data$level == row, ])
-
-  windowRanges <- make_window_ranges(dataRanges, xlab, ylab, ytickmarks=ylabels)
-  # adjust for space needed 
-#  windowRanges[1] <- windowRanges[1] - ylabel*diff(windowRanges[1:2])/600
-
-  #create the plot
-  #window size 600 x 600
-  plot1<-make_new_plot(windowRanges=windowRanges)
-
- 
-# draw grid 
-  sx <- scale_x_product(data)
-  sy <- scale_y_product(data)
-  draw_grid_with_positions(plot1, dataRanges, sx$breaks, sy$breaks, sx$minor_breaks, sy$minor_breaks)
-
-# put labels, if appropriate
-  col <- productplots:::find_col_level(data)
-  if (!is.na(col)) {
-  	labels <- productplots:::col_labels(data[data$level == col, ])
-	
-	draw_x_axes_with_labels(plot1, dataRanges, axisLabel=labels$label, labelHoriPos=labels$pos, name=xlab)
-  } else {
-  	draw_x_axes_with_labels(plot1, dataRanges, axisLabel=rep("",length(sx$breaks)), labelHoriPos=sx$breaks, name=xlab)
-  }
-
-  if (!is.na(row)) {
-  	labels <- productplots:::row_labels(data[data$level == row, ])
-  	draw_y_axes_with_labels(plot1, dataRanges, axisLabel=labels$label, labelVertPos=labels$pos, name=ylab)
-	
-  } else {
-  	draw_y_axes_with_labels(plot1, dataRanges, axisLabel=rep("",length(sy$breaks)), labelVertPos=sy$breaks, name=ylab)
-  }
-  
-
-  mousePressFun <- function(item, event, ...) {
-  
-	#browser()
-	print("mousedown")
-	pos = as.numeric(event$pos())
-	print(pos)
-  }
-  
-  hover <- function(item, event, ...) {
-  #  print(str(item$primitives(rect)))
-  
-    .queryPos <<- as.numeric(event$pos())
-#    print(.queryPos)
-  }
-
-  hover.leave <- function(item, event) {
-	.queryPos <<- NULL
-  }
-
-# data layer
-  plot1$add_layer(rect(top=top, left=left, bottom=bottom, right=right, fill=colour), hoverMoveEvent=hover, hoverEnterEvent = hover.enter, hoverLeaveEvent = hover.leave, mousePressFun=mousePressFun)
-
-  # for highlighting: draw highlighted boxes
-  if (length(grep("highlight",names(res))) > 0) {
-	hildata <- subset(data, highlight==T)
-	top <- hildata$t
-	bottom <- hildata$b
-	left <- hildata$l
-	right <- hildata$r
-    plot1$add_layer(rect(top=top, left=left, bottom=bottom, right=right, fill=highlight))
-  }
-
-# additional info required?
-  if (!is.null(.queryPos)) {
-    plot1$add_layer(rect(top=.queryPos.x, left=.queryPos.x+5, bottom=.queryPos.y, right=.queryPos.y+5, fill="pink"))	
-  }
-#  print(paste("draw, .queryPos:",.queryPos))
-
-
-
-  plot1
-}
-
-
-
-hover.enter <- function(item, event, ...) {
-  .queryPos <<- as.numeric(event$pos())
-}
-
+plot1 <- qmosaic(happy, ~ health+sex+happy, c("fluct","hspine"))  
