4accb3b98fb3b8c3861164279076bf16b10ff691
mariev <mariev@iastate.edu>
2010-08-20 09:23:25 -0500

    added qtrect

--- Marie/qtbaseANDpaint_v425/qtplots.R
+++ Marie/qtbaseANDpaint_v425/qtplots.R
@@ -4,10 +4,16 @@
 
 qtpoint<-function(parent,x,y,color='black',size=5,alpha=1.0){
   layerID<-length(parent$root$childItems())+1
-  mark<-glyph(left=x,bottom=y,fill=color,stroke=color,size=size)
-  add_layer(parent,mark,geometry=qrect(0,0,parent$root$geometry$width(),parent$root$geometry$height()))
+  mark<-glyph(left=x-parent$limits$left(),bottom=y-parent$limits$top(),fill=color,stroke=color,size=size,parent=parent)
+  add_layer(parent,mark)
   modify_layer(layerID,parent,alpha)
 }
 
 
-qtline<-function(parent,x,y,color='black',width,alpha=1.0){
+qtrect<-function(parent,x,y,color="black",width=5,height=5,alpha=1.0){
+  layerID<-length(parent$root$childItems())+1
+  mark<-rect(left=x-(0.5*width)-parent$limits$left(),bottom=y-(0.5*height)-parent$limits$top(),width=width, height=height,fill=color,stroke=color,parent=parent)
+  add_layer(parent, mark)
+  modify_layer(layerID,parent,alpha)
+}
+
