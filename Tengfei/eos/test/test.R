b11f2f3847b0b29d00738dd2240a7861dbeaaafd
tengfei <tengfei@tengfei-laptop.(none)>
2010-09-03 14:40:33 -0500

    Mouse double click to update two linked views

--- Tengfei/eos/test/test.R
+++ Tengfei/eos/test/test.R
@@ -1,11 +1,20 @@
 library(plumbr)
 library(qtpaint)
+args(qlayer)
 mydf <- mutaframe(x=rnorm(100,50,20),
                    y=rnorm(100,50,20),
-                  color=I('red'))
+                  color=I('black'))
 
 ## two scatter plots connects to this mutaframe
-
+mouseDB <- function(layer,event){
+  rect <- qrect(0,0,10,10)
+  mat <- layer$deviceTransform(event)$inverted()
+  rect <- mat$mapRect(rect)
+  pos <- event$pos()
+  rect$moveCenter(pos)
+  idx <- layer$locate(rect)+1
+  mydf$color[idx] <- I('red')
+}
 p1 <- function(layer,painter){
   qdrawCircle(painter,mydf$x,mydf$y,r=5,fill=mydf$color)
 }
@@ -15,8 +24,9 @@ p2 <- function(layer,painter){
 
 s1 <- qscene()
 s2 <- qscene()
-l1 <- qlayer(s1,p1,limits=qrect(0,0,100,100))
-l2 <- qlayer(s2,p2,limits=qrect(0,0,100,100))
+
+l1 <- qlayer(s1,p1,mouseDoubleClickFun=mouseDB,limits=qrect(0,0,100,100))
+l2 <- qlayer(s2,p2,mouseDoubleClickFun=mouseDB,limits=qrect(0,0,100,100))
 v1 <- qplotView(s1)
 v1$show()
 v2 <- qplotView(s2)
@@ -31,9 +41,9 @@ add_listener(mydf,function(i,j){
 
 
 ## Then when you change the color slots
-mydf$color[70] <- I('black')
-mydf$color[60] <- I('blue')
+## mydf$color[70] <- I('black')
+## mydf$color[60] <- I('blue')
 ## change back
-mydf$color <- I('red')
+## mydf$color <- I('red')
 
 ## both plots update
