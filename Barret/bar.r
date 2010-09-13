2ba3a51549f230f363e525fb94b444b00d3d5944
Barret Schloerke <schloerke@gmail.com>
2010-09-13 14:18:37 -0500

    cleaned hist.  it is fancy now.

--- Barret/bar.r
+++ Barret/bar.r
@@ -61,19 +61,10 @@ continuous_to_bars <- function(data = NULL, splitBy = NULL, position = "none", c
   data_pos <- melt(bar_top)
   names(data_pos) <- c("label", "group", "top")
 
-#  attributes(data_pos)$original <- original
-#  attributes(data_pos)$breaks <- breaks
-#  attributes(data_pos)$label_names <- unique(data_pos$label)
-#  attributes(data_pos)$group_names <- unique(data_pos$group)
   label_names <- unique(data_pos$label)
   group_names <- unique(data_pos$group)
 
-
   data_pos$bottom <- rep(0, nrow(data_pos))
-  
-#  bar_bottom <- array(0, dim(bar_top))
-#  label_names <- dimnames(bar_top)[[1]]
-#  split_names <- dimnames(bar_top)[[2]]
 
   if(is.null(color)) {
     if(length(group_names) == 1) {
@@ -82,9 +73,6 @@ continuous_to_bars <- function(data = NULL, splitBy = NULL, position = "none", c
       data_pos$color <- rep(rainbow(length(group_names)), each = length(label_names))
     }
   }
-
-#  bprint(label_names)
-#  bprint(split_names)
     
   if (position == "dodge") {
     
@@ -93,17 +81,9 @@ continuous_to_bars <- function(data = NULL, splitBy = NULL, position = "none", c
     data_pos$right <- pos$end
   } else  {
     # (position == "stack" || position == "relative")
-    
-    
-#    color <- rep(color, each <- length(label_names))
-#    data_pos$color <- rep(color, each = length(split_names))
 
-    #(position = "stack")
-#    bar_left <- rep(breaks[1:(break_len-1)], length(split_names))
-#    bar_right <- rep(breaks[2:break_len] , length(split_names))
     data_pos$left <- rep(breaks[1:(break_len-1)], length(group_names))
     data_pos$right <- rep(breaks[2:break_len] , length(group_names))
-    
     
     if(position != "identity") {
       # make the bar_top be stacked (cumulative)
@@ -114,40 +94,21 @@ continuous_to_bars <- function(data = NULL, splitBy = NULL, position = "none", c
     }
     
     #make the bar_bottom "stack"
-#    if (ncol(bar_bottom) > 1) {
-#      bar_bottom[,2:ncol(bar_bottom)] <- bar_top[,1:(ncol(bar_top) - 1)]
-#    }
-#    data_pos$bottom[-1] <- data_pos$top[-nrow(data_pos)]
     data_pos <- data_pos[order(data_pos$top),]
     data_pos <- ddply(data_pos, "label", transform, bottom = zero_then_top_by_order(top, order))
 
-#    bar_bottom[,1] <- 0
-    
-    
-    # spine-o-gram      
+    # relative      
     if (position == "relative") {
-#      for (i in 1:nrow(bar_bottom)) {
-#        bar_bottom[i,] <- bar_bottom[i,] / max(bar_top[i,])
-#      }
-      print(data_pos)
-
-      data_pos <- ddply(data_pos, c("label"), transform, bottom = divide_by_maximum(bottom, top), .progress="text")
-
-#      for (i in 1:nrow(bar_top)) {
-#        bar_top[i,] <- bar_top[i,] / max(bar_top[i,])
-#      }
-      data_pos <- ddply(data_pos, c("label"), transform, top = divide_by_maximum(top), .progress="text")
-      print(data_pos)
+      data_pos <- ddply(data_pos, c("label"), transform, bottom = divide_by_maximum(bottom, top))
+      data_pos <- ddply(data_pos, c("label"), transform, top = divide_by_maximum(top))
     }
   }
-#  bar_top <- c(bar_top)
-#  bar_bottom <- c(bar_bottom)
   
   # Color Management
   f_and_s <- fill_and_stroke(data_pos$color, fill = fill, stroke = stroke)
   data_pos$fill = f_and_s$fill
   data_pos$stroke = f_and_s$stroke
-#  data_pos$color = NULL
+  data_pos$color = NULL
 
   list(
     data = data_pos,
@@ -196,15 +157,7 @@ qtdot <- function(
 
   bars_info <- continuous_to_bars(data, splitBy, position, color, fill, stroke, ...)
   bars <- bars_info$data
-  bprint(bars)
-  str(bars)
   color <- bars$color  
-
-#  bprint(bars$left)
-#  bprint(bars$right)
-#  bprint(bars$top)
-#  bprint(bars$bottom)
-#  bprint(bars$color)
 
     
   # contains c(x_min, x_max, y_min, y_max)
@@ -213,8 +166,7 @@ qtdot <- function(
   } else {
     ranges <- c(make_data_ranges(bars_info$breaks), make_data_ranges( c(0, bars$top)))
   }
-  bprint(ranges)
-
+  
   if (horizontal) {
     ylab = name
     xlab = "count"
@@ -222,32 +174,27 @@ qtdot <- function(
     ylab = "count"
     xlab = name
   }
-#  bprint(xlab)
-#  bprint(ylab)
 
-  #create the plot
-  #window size 600 x 600; xrange and yrange from above
+  # create the plot
+  # window size 600 x 600; xrange and yrange from above
   windowRanges <- make_window_ranges(ranges, xlab, ylab)
   plot1<-make_new_plot(windowRanges)
 
-  #draw grid
-  if(horizontal)
+  # draw grid
+  if (horizontal) {
     draw_grid_with_positions(plot1, ranges, make_pretty_axes(ranges[1:2], ranges[1], ranges[2]), NULL)
-  else
+  } else {
     draw_grid_with_positions(plot1, ranges, NULL, make_pretty_axes(ranges[3:4], ranges[3], ranges[4]))
+  }
     
   
-  #for different representations of the data (shape, color, etc) pass vecor arguments for shape, color, x, y
-#  if(horizontal)
-#    plot1$add_layer(hbar(bottom = bars$left, top = bars$right, width = bars$top, ...))
-#  else
-#    plot1$add_layer(vbar(left = bars$left, right = bars$right, height = bars$top, ...))
-
+  # for different representations of the data (shape, color, etc) pass vecor arguments for shape, color, x, y
   # c(obj) makes a matrix into a vector
-  if(horizontal)
+  if (horizontal) {
     plot1$add_layer(hbar(bottom = c(bars$left), top = c(bars$right), width = c(bars$top), left = c(bars$bottom), fill = c(bars$fill), stroke = c(bars$stroke)))
-  else
+  } else {
     plot1$add_layer(vbar(left = c(bars$left), right = c(bars$right), height = c(bars$top), bottom = c(bars$bottom), fill = c(bars$fill), stroke = c(bars$stroke)))
+  }
 
   draw_x_axes(plot1, ranges, xlab)
   draw_y_axes(plot1, ranges, ylab) 
@@ -293,24 +240,17 @@ qthist <- function(
   ...
 ) {
   
-  bars <- continuous_to_bars(data, splitBy, position, color, fill, stroke, ...)
-  color <- bars$color 
-  bprint(bars)
-   
+  bars_info <- continuous_to_bars(data, splitBy, position, color, fill, stroke, ...)
+  bars <- bars_info$data
+  color <- bars$color  
 
-#  bprint(bars$left)
-#  bprint(bars$right)
-#  bprint(bars$top)
-#  bprint(bars$bottom)
-#  bprint(bars$color)
-  
+    
   # contains c(x_min, x_max, y_min, y_max)
   if (horizontal) {
-    ranges <- c(make_data_ranges(c(0, bars$top)), make_data_ranges(attr(bars, "breaks")))
+    ranges <- c(make_data_ranges(c(0, bars$top)), make_data_ranges(bars_info$breaks))
   } else {
-    ranges <- c(make_data_ranges(attr(bars, "breaks")), make_data_ranges( c(0, bars$top)))
+    ranges <- c(make_data_ranges(bars_info$breaks), make_data_ranges( c(0, bars$top)))
   }
-#  bprint(ranges)
 
   if (horizontal) {
     ylab = name
@@ -319,32 +259,27 @@ qthist <- function(
     ylab = "count"
     xlab = name
   }
-#  bprint(xlab)
-#  bprint(ylab)
 
-  #create the plot
-  #window size 600 x 600; xrange and yrange from above
+  # create the plot
+  # window size 600 x 600; xrange and yrange from above
   windowRanges <- make_window_ranges(ranges, xlab, ylab)
   plot1<-make_new_plot(windowRanges)
 
-  #draw grid
-  if(horizontal)
+  # draw grid
+  if (horizontal) {
     draw_grid_with_positions(plot1, ranges, make_pretty_axes(ranges[1:2], ranges[1], ranges[2]), NULL)
-  else
+  } else {
     draw_grid_with_positions(plot1, ranges, NULL, make_pretty_axes(ranges[3:4], ranges[3], ranges[4]))
+  }
     
   
-  #for different representations of the data (shape, color, etc) pass vecor arguments for shape, color, x, y
-#  if(horizontal)
-#    plot1$add_layer(hbar(bottom = bars$left, top = bars$right, width = bars$top, ...))
-#  else
-#    plot1$add_layer(vbar(left = bars$left, right = bars$right, height = bars$top, ...))
-
+  # for different representations of the data (shape, color, etc) pass vecor arguments for shape, color, x, y
   # c(obj) makes a matrix into a vector
-  if(horizontal)
-    plot1$add_layer(hbar(bottom = c(bars$left), top = c(bars$right), width = c(bars$top), left = c(bars$bottom), fill=bars$fill, stroke = bars$stroke))
-  else
-    plot1$add_layer(vbar(left = c(bars$left), right = c(bars$right), height = c(bars$top), bottom = c(bars$bottom), fill=bars$fill, stroke = bars$stroke))
+  if (horizontal) {
+    plot1$add_layer(hbar(bottom = c(bars$left), top = c(bars$right), width = c(bars$top), left = c(bars$bottom), fill = c(bars$fill), stroke = c(bars$stroke)))
+  } else {
+    plot1$add_layer(vbar(left = c(bars$left), right = c(bars$right), height = c(bars$top), bottom = c(bars$bottom), fill = c(bars$fill), stroke = c(bars$stroke)))
+  }
 
   draw_x_axes(plot1, ranges, xlab)
   draw_y_axes(plot1, ranges, ylab) 
