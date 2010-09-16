24c0a042cc33c5a25be58ddc777d986cdaf8e5c9
hadley <h.wickham@gmail.com>
2010-09-16 09:15:17 -0400

    Move labels into own functions

--- Heike/mosaic.r
+++ Heike/mosaic.r
@@ -1,25 +1,9 @@
 source("../utilities/api-sketch.r")
 source("../utilities/helper.r")
 source("../utilities/axes.r")
+source("labels.r")
 require(stringr)
 require(productplots)
-
-find_x_label <- function(df) {
-  vars <- setdiff(names(df), c(".wt", "l", "r", "t", "b", "level"))
-
-  axis.set <- subset(df, (b==min(b)) &  (level==max(level)))
-  
-  paste(vars[sapply(vars, function(x) return(length(unique(axis.set[,x]))>1))],"")
-}
-
-find_y_label <- function(df) {
-  vars <- setdiff(names(df), c(".wt", "l", "r", "t", "b", "level"))
-
-  axis.set <- subset(df, (l==min(l)) & (level==max(level)))
-  
-  paste(vars[sapply(vars, function(x) return(length(unique(axis.set[,x]))>1))],"")
-}
-
 
 qmosaic <- function(data, formula, divider = mosaic(), cascade = 0, scale_max = TRUE, na.rm = FALSE, subset=NULL, colour="grey30", main=NULL, ...) {
   odata <- data
