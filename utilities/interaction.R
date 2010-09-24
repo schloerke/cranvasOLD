b6e5c4280f9a26c7f25050030a35f51e9a435b72
Yihui Xie <xie@yihui.name>
2010-09-24 13:07:53 -0500

    reconstruct the qmutaframe() function -- row attributes are augmented to the original data now

--- utilities/interaction.R
+++ utilities/interaction.R
@@ -1,22 +1,33 @@
-## add attributes (mutaframes) to data for future interaction
-qmutaframe = function(data) {
-    # check if the attribute exists
-    has_attr = function(which) {
-	!is.null(attr(data, which))
+###
+##' Create a Mutaframe from Data with Several Attributes for Future Interaction
+##'
+##' Create a Mutaframe from Data with Several Attributes for Future Interaction
+##' @title Create a Mutaframe from Data with Several Attributes for Future Interaction
+##' @param data a data frame (typically); it will be coerced to a data frame
+##' @param ... other attributes corresponding to rows such as colours, sizes and so on
+##' @return a mutaframe
+##' @author Yihui Xie
+qmutaframe = function(data, ...) {
+    if (!is.data.frame(data)) data = as.data.frame(data)
+    ## check if the attribute exists
+    all_in = function(x1, x2) {
+	sapply(x1, '%in%', x2)
     }
-
-    data = as.data.frame(data)
-    data = as.mutaframe(data)
-    n = nrow(data)
-
-    if (!has_attr("row.attr")) {
-	row.attr = mutaframe(.color = rep("black", n), .size = rep(1L, n), .shape = rep(1L, n), .brushed = logical(n))
-	attr(data, "row.attr") = row.attr
+    ## row attributes needed by all plotting functions
+    row_attrs=c('.color', '.size', '.brushed')
+    ## once in a blue moon...
+    conflict_attrs = all_in(row_attrs, colnames(data))
+    if(any(conflict_attrs)) {
+        stop(sprintf('variable names conflicts: %s already exist(s) in data',
+                     paste(row_attrs[conflict_attrs], collapse = ', ')))
     }
+    ## all possible row attributes that _might_ be needed
+    ## row_attrs=c(row_attrs, '.shape', '.ltype')
+    ## dot_names=names(list(...))
+    ## if (!is.null(dot_names) && all_in(row_attrs, dot_names)) {
 
-    data
-}
-## query row attributes
-get_row_attr = function(data) {
-    attr(data, "row.attr")
+    ## }
+    mf = mutaframe(data, ...)
+
+    mf
 }
