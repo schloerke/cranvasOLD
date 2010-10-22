1f7626958a6dcf3c579f357395e43a55b10126f9
mariev <mariev@iastate.edu>
2010-10-22 04:14:38 -0500

    added window size max limits

--- Marie/qscatter.r
+++ Marie/qscatter.r
@@ -244,7 +244,7 @@ brush.draw <- function(item, painter, exposed) {
   brushlayer <- add_layer(parent = plot1, mark = brush.draw, userlimits = lims)
   view <- qplotView(scene = plot1$scene)
   view$setWindowTitle(extract.formula(form))
-
+  view$setMaximumSize(plot1$size)
 ######################
 # add some listeners #
 ######################
