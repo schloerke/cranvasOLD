3bd10c051bde4bb9c2c3fba9a6cd8653a9ce7005
Yihui Xie <xie@yihui.name>
2010-11-12 13:33:28 -0600

    I can use integers to subset columns now

--- files/Yihui/nrc-stat.R
+++ files/Yihui/nrc-stat.R
@@ -5,15 +5,14 @@ library(qtpaint)
 data(nrcstat)
 ## names(nrc)[8]='Regional.Code'
 ## nrc$Regional.Code=factor(nrc$Regional.Code, labels=c('NE','MW','SA','SC', 'W'))
-nms = names(nrcstat)
 
 qnrc = qmutaframe(nrcstat)
 rownames(qnrc)=paste(nrcstat$Institution.Name, nrcstat$Program.Name, sep = ' -> ')
 
 ## Overview: type, rankings
-qparallel(qnrc, vars = nms[7:13], main = 'Overview of Rankings')
+qparallel(qnrc, vars = 7:13, main = 'Overview of Rankings')
 qnrc$.color = 'red'
-qparallel(qnrc, vars = nms[7:13], main = 'Overview of Rankings', glyph='tick', center=median, boxplot=TRUE, order = TRUE)
+qparallel(qnrc, vars = 7:13, main = 'Overview of Rankings', glyph='tick', center=median, boxplot=TRUE, order = TRUE)
 
 ## How to find out ISU by intersection and negation? public, midwest, large program
 
@@ -25,8 +24,8 @@ brush_attr(qnrc, '.label.show') = FALSE
 
 qnrc$.color = 'red'
 
-qparallel(qnrc, vars = nms[14:19], main = 'Research, Student Support, Diversity')
+qparallel(qnrc, vars = 14:19, main = 'Research, Student Support, Diversity')
 
-qparallel(qnrc, vars = nms[20:26], main = 'Publication, Award, Time to Degree')
+qparallel(qnrc, vars = 20:26, main = 'Publication, Award, Time to Degree')
 
 
