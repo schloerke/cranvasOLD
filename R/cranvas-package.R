c98face40746c879145b6909f8eea9b6af1a3084
Yihui Xie <xie@yihui.name>
2010-12-15 09:10:55 -0600

    move namespace specifications to roxygen (see cranvas-package.R)

--- R/cranvas-package.R
+++ R/cranvas-package.R
@@ -1,3 +1,16 @@
+##' Interactive Statistical Graphics Based on Qt
+##'
+##' @importFrom qtbase Qt
+##' @importFrom qtbase qrect qfont
+##' @import qtpaint
+##' @import reshape
+##' @import plyr
+##' @import plumbr
+##' @exportPattern "^[^\\.]"
+##' @name cranvas-package
+##' @docType package
+NULL
+
 ##' NRC Rankings Data for the Statistics Departments in US.
 ##' This data contains the NRC rankings for all the statistics departments in US.
 ##'
